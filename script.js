717236588c5b34588dc94e8f8b9a6acba7205c04
joep-web <133377772+joep-web@users.noreply.github.com>
2023-12-29 15:06:52 +0000

    removed erase btn, changed layout, done clear btn

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gridCountText = document.querySelector("#grid-count");
 const gridRange = document.querySelector("input[type='range']");
 const contGrid = document.querySelector(".grid");
+const clearAllColor = document.querySelector("#clear");
+const colorPicker = document.querySelector("#color");
+
 
 const getGridSize = () => {
   const gridSizeText = document.querySelector("#grid-count").innerText;
@@ -8,12 +11,6 @@ const getGridSize = () => {
   return gridSize;
 };
 
-gridRange.addEventListener("input", (e) => {
-  gridCountText.innerText = `${e.target.value}x${e.target.value}`;
-  getGridSize();
-  gridReset();
-  gridSetUp(getGridSize());
-});
 
 const gridReset = () => {
   const allExistingGrid = contGrid.querySelectorAll("div");
@@ -28,7 +25,34 @@ const gridSetUp  = (gridSize) => {
 
   for(let i = 0; i < gridSize * gridSize; i++) {
     const newDivs = document.createElement("div");
-    newDivs.classList.add("each-div");
+    newDivs.classList.add("each-div", "default");
     contGrid.append(newDivs);
   }
 };
+
+gridRange.addEventListener("input", (e) => {
+  gridCountText.innerText = `${e.target.value}x${e.target.value}`;
+  getGridSize();
+  gridReset();
+  gridSetUp(getGridSize());
+});
+
+const backgroundToggles = (e) => {
+  e.target.classList.toggle("default");
+  e.target.classList.toggle("black");
+}
+
+contGrid.addEventListener("click", (e) => {
+  backgroundToggles(e);
+});
+
+clearAllColor.addEventListener("click", () => {
+  contGrid.querySelectorAll("div").forEach((div) => {
+    div.classList.replace("black", "default");
+  });
+});
+
+ 
+onload = () => {
+  gridSetUp(getGridSize());
+}
