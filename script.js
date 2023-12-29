9e1f18f5def6e68c5c98005b913c455c9fe0fa5f
joep-web <133377772+joep-web@users.noreply.github.com>
2023-12-29 16:55:38 +0000

    done everything

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const gridRange = document.querySelector("input[type='range']");
 const contGrid = document.querySelector(".grid");
 const clearAllColor = document.querySelector("#clear");
 const colorPicker = document.querySelector("#color");
+let colorCondition = false;
 
 
+//             Functions
 const getGridSize = () => {
   const gridSizeText = document.querySelector("#grid-count").innerText;
   const gridSize = Number(gridSizeText.split("x")[0]);
   return gridSize;
 };
-
 
 const gridReset = () => {
   const allExistingGrid = contGrid.querySelectorAll("div");
@@ -25,7 +26,8 @@ const gridSetUp  = (gridSize) => {
 
   for(let i = 0; i < gridSize * gridSize; i++) {
     const newDivs = document.createElement("div");
-    newDivs.classList.add("each-div", "default");
+    newDivs.style.backgroundColor = "white";
+    newDivs.style.border = "1px solid #282B2B";
     contGrid.append(newDivs);
   }
 };
@@ -37,21 +39,37 @@ gridRange.addEventListener("input", (e) => {
   gridSetUp(getGridSize());
 });
 
-const backgroundToggles = (e) => {
-  e.target.classList.toggle("default");
-  e.target.classList.toggle("black");
-}
-
 contGrid.addEventListener("click", (e) => {
-  backgroundToggles(e);
+  if(e.target.style.backgroundColor === "white"){
+    if (colorCondition === false) {
+      e.target.style.backgroundColor = "black";
+    } else {
+      //random picker
+      let randomColor = Math.floor(Math.random()*360);
+      e.target.style.backgroundColor = `hsl(${randomColor}, 47%, 67%)`;
+    }
+  } else {
+    e.target.style.backgroundColor = "white";
+  }
 });
 
 clearAllColor.addEventListener("click", () => {
   contGrid.querySelectorAll("div").forEach((div) => {
-    div.classList.replace("black", "default");
+    div.style.backgroundColor = "white";
   });
 });
 
+colorPicker.addEventListener("click", (e) => {
+  if(colorCondition === false) {
+    colorCondition = true;
+    e.target.style.background = "linear-gradient(45deg, hsl(120, 47%, 67%), hsl(240, 47%, 67%), hsl(45, 47%, 67%), hsl(300, 47%, 67%))";
+    e.target.style.color = "white";
+  } else {
+    colorCondition = false;
+    e.target.style.background = "#FFFDF9";
+    e.target.style.color = "#392B0B";
+  }
+});
  
 onload = () => {
   gridSetUp(getGridSize());
